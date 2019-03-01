6bc887aac753c8f9e4dcdfa798f9ca2b8d82c8bc
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2019-03-01 01:07:20 +0000

    Add publish script since it's long and complicated

---
+++ vscode/publish.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+vsce publish --baseImagesUrl https://raw.githubusercontent.com/wandwramp/syntax-highlighting/master/vscode --baseContentUrl https://github.com/wandwramp/syntax-highlighting/tree/master/vscode
